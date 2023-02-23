af12ca368ca77be8a126278873ff5c2d5e8793ce
arvind <developer.arvind003@gmail.com>
2023-02-23 17:05:33 +0530

    gitignore

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost:27017', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017",{useNewUrlParser:true,useUnifiedTopology:true})
   .then(() => {
-    console.log('connected');
-  })
-  .catch((err) => {
+	  console.log('connected');
+	})
+	.catch((err) => {
+	  console.log('error is happening');
     console.log(err);
   });
